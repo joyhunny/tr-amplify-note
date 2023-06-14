96013056cc9ac54b425e9ec1e4473231fe84906c
joyhunny <joyhunny@gmail.com>
2023-06-14 10:22:49 +0900

    add tutorial code

---
+++ src/ui-components/index.js
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+export { default as Features2x3 } from "./Features2x3";
+export { default as TallCard } from "./TallCard";
+export { default as HeroLayout1 } from "./HeroLayout1";
+export { default as NoteUI } from "./NoteUI";
+export { default as FeaturesText2x2 } from "./FeaturesText2x2";
+export { default as Ampligram } from "./Ampligram";
+export { default as PhotoNote } from "./PhotoNote";
+export { default as ProductCard } from "./ProductCard";
+export { default as MyIcon } from "./MyIcon";
+export { default as CreateNote } from "./CreateNote";
+export { default as Features4x1 } from "./Features4x1";
+export { default as ActionCard } from "./ActionCard";
+export { default as HeroLayout2 } from "./HeroLayout2";
+export { default as UpdateNote } from "./UpdateNote";
+export { default as NavBar } from "./NavBar";
+export { default as MarketingPricing } from "./MarketingPricing";
+export { default as StandardCard } from "./StandardCard";
+export { default as Features2x2 } from "./Features2x2";
+export { default as HeroLayout4 } from "./HeroLayout4";
+export { default as HeroLayout3 } from "./HeroLayout3";
+export { default as NoteUICollection } from "./NoteUICollection";
+export { default as studioTheme } from "./studioTheme";
+export { default as NoteCreateForm } from "./NoteCreateForm";
+export { default as NoteUpdateForm } from "./NoteUpdateForm";
